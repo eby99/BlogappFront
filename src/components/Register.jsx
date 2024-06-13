948a42300cae494ad98d79f70f8dd33595e2b34e
eby99 <ebymathew619@gmail.com>
2024-06-13 15:52:40 +0530

    Compelted

---
+++ src/components/Register.jsx
@@ -0,0 +1,64 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+
+const Register = () => {
+    const [data, setData] = useState(
+        {
+            "name":"",
+            "email":"",
+            "passwd":""
+        }
+    )
+    const inputHandler = (event) => {
+        setData({ ...data, [event.target.name]: event.target.value })
+    }
+    const readValue = () => {
+        console.log(data)
+        axios.post("http://localhost:8080/signup", data).then(
+            (response) => {
+                console.log(response.data)
+                if (response.data.status == "success") {
+                    alert("Successfully Registered")
+                }
+                else {
+                    alert("Error")
+                }
+            }
+
+        ).catch(
+            (error) => {
+                console.log(error.message)
+                alert(error.message)
+            }
+        )
+    }
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="label form-label">User Name</label>
+                            <input type="text" className="input form-control" name='name' value={data.name} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="label form-label">Email</label>
+                        <input type="text" className="input form-control" name='email' value={data.email} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="label form-label">Password</label>
+                        <input type="password" className="input form-control" name='passwd' value={data.passwd} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-danger" onClick={readValue}>Register</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Register
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,41 +1,73 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Signin = () => {
-  return (
-    <div>
-        <div className="container1-fluid">
-            <h2>SnapBook</h2>
-                
-            <img  class="img-fluid"
-          src="https://clevertap.com/wp-content/uploads/2019/07/FTUE-blog-header1.png"  width="100%"/>
-          
-       <div className="container1">
-            <div className="row g-3">
-                <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6 ">
-                    <label htmlFor="" className="label form-label">Enter Email Id</label>
-                    <input type="text" className="form-control" placeholder='email-id' />
-                </div>
-                <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6">
-                </div>
-                <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6">
-                    <label htmlFor="" className="label form-label">Enter Password</label>
-                    <input type="text" className="form-control" placeholder='password'/>
-                </div>
-                <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6">
-                </div>
-                <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6">
-                    <button className="btn btn-primary">Signin</button>
-                </div>
-                <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6">
-                </div>
-                <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6">
-                   <a href='/Register'> Register</a>
-                </div>
+    const [data, setData] = useState(
+        {
+            "name":"",
+            "email":"",
+            "passwd":""
+        }
+    )
+    const inputHandler = (event) => {
+        setData({ ...data, [event.target.name]: event.target.value })
+    }
+    const readValue = () => {
+        console.log(data)
+        axios.post("http://localhost:8080/login", data).then(
+            (response) => {
+                console.log(response.data)
+                if (response.data.status == "success") {
+                    alert("Successfully Registered")
+                }
+                else {
+                    alert("Not Registered")
+                }
+            }
+
+        ).catch(
+            (error) => {
+                console.log(error.message)
+                alert(error.message)
+            }
+        )
+    }
+    return (
+        <div>
+            <div className="container1-fluid">
+                <h2> New Project </h2>
+
+                <img class="img-fluid"
+                    src="https://wallpapercave.com/wp/wp2939867.png" width="100%" />
+
+                <div className="container1">
+                    <div className="row g-3">
+                        <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6 ">
+                            <label htmlFor="" className="label form-label">Enter Email Id</label>
+                            <input type="text" className="form-control" placeholder='email' name='email' value={data.email} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6">
+                        </div>
+                        <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="label form-label">Enter Password</label>
+                            <input type="password" className="form-control" placeholder='password'  name='passwd' value={data.passwd} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6">
+                        </div>
+                        <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6">
+                        <button className="btn btn-danger" onClick={readValue}>Register</button>
+                        </div>
+                        <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6">
+                        </div>
+                        <div className="col col-6 col-sm-6 col-md-6 col-xl-6 col-xxl-6">
+                            <a href='/Register'> Sign Up</a>
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </div>
-    </div>
-  )
+    )
 }
 
 export default Signin
